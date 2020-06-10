878b9ea54b5e2eb72ab01e10dfe56408ebf9f08a
7happy7 - JP <49482246+7happy7@users.noreply.github.com>
2020-06-10 23:00:24 +0900

    Create remover.js

---
+++ js/remover.js
@@ -0,0 +1,24 @@
+window.onload=function() {
+    (async function _f(){
+        var a=(v)=>document.querySelector(v);
+        var b=a("video");
+        if(!b && document.domain=="www.youtube.com"){
+            return await new Promise(r=>(setTimeout(r, 500), _f()));
+        }
+        b.ontimeupdate=()=>{
+            try{
+                var d=a(".ytp-ad-skip-button-slot button,.ytp-ad-overlay-close-button");
+                if(d){
+                    d.click();
+                }
+            }catch(e){}
+            try{
+                var c=a(".ad-showing");
+                var e=b.duration;
+                if(c){
+                    b.currentTime=e;
+                }
+            }catch(e){}
+        }
+    })();
+}
