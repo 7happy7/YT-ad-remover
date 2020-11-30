688989a4cda6ece3917db8d2daf5cec051eeeb91
7happy7 - JP <49482246+7happy7@users.noreply.github.com>
2020-12-01 03:15:13 +0900

    Update remover.js

--- js/remover.js
+++ js/remover.js
@@ -1,24 +1,25 @@
-window.addEventListener("load",function() {
-    (function _f(){
-        var a=(v)=>document.querySelector(v);
-        var b=a("video");
-        if(!b){
-            return setTimeout(_f, 500);
-        }
-        b.addEventListener("ontimeupdate",function() {
-            try{
-                var d=a(".ytp-ad-skip-button-slot button,.ytp-ad-overlay-close-button");
-                if(d){
-                    d.click();
-                }
-            }catch(e){}
-            try{
-                var c=a(".ad-showing");
-                var e=b.duration;
-                if(c){
-                    b.currentTime=e;
-                }
-            }catch(e){}
-        });
-    })();
-});
+(async(x,f,e,r,s)=>{
+    (await new Promise(r=>document.readyState=='complete'?r():window.addEventListener('load',r))),
+    (r=(a,b,c)=>{
+        return(b=a('video'))?(b.ontimeupdate=_=>{
+            e(d=>(
+                (d=a('.ytp-ad-skip-button-slot button,.ytp-ad-overlay-close-button'))&&d.click()
+            )),
+            e(c=>(
+                a('.ytp-ad-skip-button-slot button,.ytp-ad-overlay-close-button')&&(b.currentTime=b.duration)
+            ))
+        })
+        :setTimeout(r,500)
+    })(_=>document.querySelector(_)),
+    f(s),
+    (s=>(x.send=function(_){_&&f(),s.call(this,_)}))(x.send)
+})(
+    XMLHttpRequest.prototype,
+    _=>(
+        (s=_||new CSSStyleSheet).replaceSync(
+            `${[...document.querySelectorAll('body *')].filter(e=>e.tagName.match(/ads?\-renderer$/i)).map(v=>v.tagName.toLowerCase()).join(',')}{position:fixed;right:9999rem}`
+        ),
+        document.adoptedStyleSheets=[s]
+    ),
+    _=>{try{_()}catch(e){}}
+)
