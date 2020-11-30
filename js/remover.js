c0a7ac62eb1b27a09081299e8d8b4358dbf49c47
7happy7 - JP <49482246+7happy7@users.noreply.github.com>
2020-12-01 03:26:14 +0900

    Update remover.js

--- js/remover.js
+++ js/remover.js
@@ -1,18 +1,20 @@
 (async(x,f,e,r,s)=>{
     (await new Promise(r=>document.readyState=='complete'?r():window.addEventListener('load',r))),
     (r=(a,b,c)=>{
-        return(b=a('video'))?(b.ontimeupdate=_=>{
-            e(d=>(
-                (d=a('.ytp-ad-skip-button-slot button,.ytp-ad-overlay-close-button'))&&d.click()
-            )),
-            e(c=>(
-                a('.ytp-ad-skip-button-slot button,.ytp-ad-overlay-close-button')&&(b.currentTime=b.duration)
-            ))
-        })
+        return(b=a('video'))?(
+            b.addEventListener('ontimeupdate',_=>{
+                e(d=>(
+                    (d=a('.ytp-ad-skip-button-slot button,.ytp-ad-overlay-close-button'))&&d.click()
+                )),
+                e(c=>(
+                    a('.ytp-ad-skip-button-slot button,.ytp-ad-overlay-close-button')&&(b.currentTime=b.duration)
+                ))
+            }),
+            (s=>(x.send=function(_){_&&f(),s.call(this,_)}))(x.send)
+        )
         :setTimeout(r,500)
     })(_=>document.querySelector(_)),
-    f(s),
-    (s=>(x.send=function(_){_&&f(),s.call(this,_)}))(x.send)
+    f(s)
 })(
     XMLHttpRequest.prototype,
     _=>(
