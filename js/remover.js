57258e3c2c8fc9efff7246106f48672fa48a5bcb
7happy7 - JP <49482246+7happy7@users.noreply.github.com>
2020-12-01 11:21:49 +0900

    Update remover.js

--- js/remover.js
+++ js/remover.js
@@ -12,7 +12,7 @@
             }),
             (t=>(x.send=function(_){_&&f(s),t.call(this,_)}))(x.send)
         )
-        :setTimeout(r,500)
+        :setTimeout(r.bind(this,a),500)
     })(_=>document.querySelector(_)),
     f(s)
 })(
