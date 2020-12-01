a0259e12a1968c3c015308b3d6737bea5f36d38a
7happy7 - JP <49482246+7happy7@users.noreply.github.com>
2020-12-01 16:33:04 +0900

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', ()=>{
         var s = document.createElement('script');
         s.setAttribute('type', 'text/javascript');
-        s.setAttribute('src', chrome.extension.getURL('/js/inject.js'));
+        s.setAttribute('src', chrome.extension.getURL('/js/remover.js'));
         document.body.appendChild(s);
 }, false);
