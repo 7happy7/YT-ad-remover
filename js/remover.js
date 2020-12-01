db4bd01173ee37a27a10dc60539e444902288767
7happy7 - JP <49482246+7happy7@users.noreply.github.com>
2020-12-01 11:35:40 +0900

    Update remover.js

--- js/remover.js
+++ js/remover.js
@@ -1,27 +1,27 @@
 (async(x,f,e,r,s)=>{
     (await new Promise(r=>document.readyState=='complete'?r():window.addEventListener('load',r))),
-    (r=(a,b,c)=>{
+    (r=(a,b)=>{
         return(b=a('video'))?(
             b.addEventListener('timeupdate',_=>{
                 e(d=>(
                     (d=a('.ytp-ad-skip-button-slot button,.ytp-ad-overlay-close-button'))&&d.click()
                 )),
-                e(c=>(
+                e(_=>(
                     a('.ytp-ad-skip-button-slot button,.ytp-ad-overlay-close-button')&&(b.currentTime=b.duration)
                 ))
             }),
             (t=>(x.send=function(_){_&&f(s),t.call(this,_)}))(x.send)
         )
         :setTimeout(r,350,a)
-    })(_=>document.querySelector(_)),
+    })(s=>document.querySelector(s)),
     f(s)
 })(
     XMLHttpRequest.prototype,
-    _=>(
-        (s=_||new CSSStyleSheet).replaceSync(
+    c=>(
+        (s=c||new CSSStyleSheet).replaceSync(
             `${[...document.querySelectorAll('body *')].filter(e=>e.tagName.match(/ads?\-renderer$/i)).map(v=>v.tagName.toLowerCase()).join(',')}{position:fixed;right:9999rem}`
         ),
         document.adoptedStyleSheets=[s]
     ),
-    _=>{try{_()}catch(e){}}
+    f=>{try{f()}catch(e){}}
 )
