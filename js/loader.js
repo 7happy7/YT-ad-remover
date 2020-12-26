8f7fe84160bb9aa43cdf178999207e5ff968d2ee
7happy7 - JP <49482246+7happy7@users.noreply.github.com>
2020-12-26 19:42:41 +0900

    Delete loader.js

--- js/loader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', ()=>{
-        var s = document.createElement('script');
-        s.setAttribute('type', 'text/javascript');
-        s.setAttribute('src', chrome.extension.getURL('/js/remover.js'));
-        document.body.appendChild(s);
-}, false);
