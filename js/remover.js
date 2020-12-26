e80a43fea74f1a11e9a8f51676927862ec728478
7happy7 - JP <49482246+7happy7@users.noreply.github.com>
2020-12-26 15:06:51 +0900

    Update remover.js

--- js/remover.js
+++ js/remover.js
@@ -1,40 +1,147 @@
 ((glb,dl,ads)=>{
     glb._audioEP = {};
     glb._audioEP.itag = {};
+
+    var wrap = document.createElement('div');
+    document.body.appendChild(wrap);
+    wrap.classList.add('aep_wrap');
+
     glb._audioEP.btn = document.createElement('button');
-    document.body.appendChild(glb._audioEP.btn);
+    wrap.appendChild(glb._audioEP.btn);
     glb._audioEP.btn.innerText = 'mp3';
-    glb._audioEP.btn.setAttribute('style','background:#065fd4;color:#fff;font-size:1.5em;font-weight:bold;position:fixed;bottom:0;left:0;outline: none!important;padding: .5em 1em;border: none!important;box-shadow: 0 0 3px #065fd4;cursor: pointer;');
+    glb._audioEP.btn.classList.add('aep_btn');
+
+    var lab = document.createElement('label');
+    wrap.appendChild(lab);
+    lab.classList.add('aep_lab');
+
+    glb._audioEP.cont_check = document.createElement('input');
+    lab.appendChild(glb._audioEP.cont_check);
+    glb._audioEP.cont_check.type = 'checkbox';
+
+    var span = document.createElement('span');
+    lab.appendChild(span);
+    span.innerText = 'controller';
+
+    glb._audioEP.cont = document.createElement('div');
+    document.body.appendChild(glb._audioEP.cont);
+    glb._audioEP.cont.classList.add('sc_log', 'sc_final');
+
+    glb._audioEP.style = new CSSStyleSheet;
+    glb._audioEP.style.replaceSync(`ytd-player-legacy-desktop-watch-ads-renderer {position: fixed;right: 9999rem;}.aep_wrap {background: #fff;bottom: 0;box-shadow: 1px 1px 3px #aaa;left: 0;padding: 0 1em 0 0;position: fixed;}.aep_lab {margin: 0 0 0 1em;}.aep_lab input {display: none;}.aep_lab input:checked + span::after {opacity: 1;transform: rotate(45deg) scale3d(1,1,1);}.aep_lab input:checked + span::before {border-color: #666;}.aep_lab span {box-sizing: border-box;cursor: pointer;display: inline-block;font-size: 14px;padding: 5px 0 5px 30px;position: relative;width: auto;}.aep_lab span::after {border-bottom: 3px solid #b01;border-right: 6px solid #b01;content: '';display: block;height: 20px;left: 7px;margin: -17px 0 0 0;opacity: 0;position: absolute;top: 50%;transform: rotate(45deg) translate3d(0,2px,0) scale3d(.7,.7,1);transition: all .2s ease-in-out;width: 9px;}.aep_lab span::before {background: #fff;box-shadow: 1px 1px 3px #aaa;content: '';display: block;height: 16px;left: 5px;margin: -9px 0 0 0;position: absolute;top: 50%;width: 16px;}.aep_btn {background: #065fd4;border: none!important;box-shadow: 0 0 3px #065fd4;color: #fff;cursor: pointer;font-size: 1.5em;font-weight: bold;outline: none!important;padding: .5em 1em;}.sc_log {animation: none;background: rgba(50,50,50,.75);bottom: 0;color: #fff;font-size: 1.3em;font-weight: bold;height: 3em;left: 0;margin: auto;max-width: 500px;opacity: 1;padding: .5em;pointer-events: none;position: fixed;right: 0;text-decoration: underline dotted;text-shadow: 1px 1px 3px #000;}.sc_log.sc_final {animation: sc_close .5s 2s ease both;opacity: 1;text-decoration: none;}@keyframes sc_close {0%,10% {opacity: 1;}90%,100% {opacity: 0;}}`);
+    document.adoptedStyleSheets = [...document.adoptedStyleSheets, glb._audioEP.style];
+
     glb._audioEP.btn.onclick = e=>{
         var t = glb._audioEP.itag[ Object.keys(glb._audioEP.itag).sort((a,b)=>a<b?1:-1)[0] ];
         t && t.length==1 ? (t[0].d()) : (confirm(`Caution: ${t.length} audio files detected. Please check out each "file size" on next dialogs.`) && t.forEach(v=>v.d()));
-    }
+    };
+
+    document.addEventListener('fullscreenchange', e=>{
+        wrap.style.display = document.fullscreenElement ? 'none' : 'block'
+    });
+
     (async(x,f,m,e,r,s)=>{
         (await new Promise(r=>document.readyState=='complete'?r():window.addEventListener('load',r))),
-        (r=(a,b,c)=>{
+        (r=(a,b,c,d)=>{
             return(b=a('video'))?(
+                c = new f(glb._audioEP.cont,{
+                    do:[
+                        {name: 'backward', reg: /(?:巻き|)戻(?:し|す|せ|って)/, key: true},
+                        {name: 'forward', reg: /進(?:み|む|め|んで)|先?送(?:り|る|れ|って)/, key: true},
+                        {name: 'repeat_', reg: /^リピート./, key: true},
+                        {name: 'repeat', reg: /^リピート$/, key: false},
+                        {name: 'initial', reg: /最初から/, key: false},
+                        {name: 'pause', reg: /ストップ|ポーズ|停止/, key: false},
+                        {name: 'play', reg: /スタート|プレイ|再生/, key: false},
+                    ],
+                    key:[
+                        {name: 'sec', reg: /(\d+)秒/},
+                        {name: 'bool', reg: /(on|off|オン|オフ)/i}
+                    ],
+                    command:{
+                        backward: {
+                            sec: (_,s)=>(b.currentTime = b.currentTime - Number(s), ` ["${s}"秒戻る]`)
+                        },
+                        forward: {
+                            sec: (_,s)=>(b.currentTime = b.currentTime + Number(s), ` ["${s}"秒進む]`)
+                        },
+                        repeat_: {
+                            bool: (_,s)=>(s = s.toLowerCase(), d = s=='on'||s=='オン', ` [リピート"${d}"]`)
+                        },
+                        repeat: (_)=>(d = true, ' [リピート"true"]'),
+                        initial: (_)=>(b.currentTime = 0, ' [最初から]'),
+                        pause: (_)=>(b.pause(), ' [一時停止]'),
+                        play: (_)=>(b.play(), ' [再生]')
+                    }
+                }),
+                glb._audioEP.cont_check.addEventListener('change', e =>{
+                    e.target.checked ? c.start() : c.stop()
+                }),
+                setInterval(()=>e(d=>(
+                    (d=a('.ytp-ad-skip-button-slot button,.ytp-ad-overlay-close-button'))&&d.click()
+                )),500),
                 b.addEventListener('timeupdate',_=>{
-                    e(d=>(
-                        (d=a('.ytp-ad-skip-button-slot button,.ytp-ad-overlay-close-button'))&&d.click()
-                    )),
-                    e(c=>(
-                        a('.ad-showing')&&(b.currentTime=b.duration)
+                    e(()=>(
+                        a('.ad-showing') ? (b.currentTime=b.duration) : (b.duration<=b.currentTime && d && (b.currentTime = 0, b.play()))
                     ))
                 }),
-                (t=>(x.send=function(_){t.call(this,_), this.addEventListener('load',_?f:m)}))(x.send),
+                (t=>(x.send=function(_){t.call(this,_), _ || this.addEventListener('load',m)}))(x.send),
                 (t=>(x.open=function(){(a=>a[1].match(ads) ? (console.log(`Blocked: "${a[1]}"`), this.abort && this.abort()) : t.call(this,...a))(arguments)}))(x.open)
             )
             :setTimeout(r,500,a)
-        })(_=>document.querySelector(_)),
-        f()
+        })(_=>document.querySelector(_))
     })(
         window.XMLHttpRequest.prototype,
-        (_,x)=>(
-            (x=[...document.querySelectorAll('body *')].filter(e=>e.tagName.match(/[^a-z]ads?\-renderer$/i)).map((v,i,a)=>(v.tagName.toLowerCase())).join(',')),
-            x&&((_=new CSSStyleSheet).replaceSync(
-                `${x}{position:fixed;right:9999rem}`
-            ),document.adoptedStyleSheets=[_])
-        ),
+        class {
+            constructor(e,o) {
+                this.r = new webkitSpeechRecognition;
+                this.r.continuous = !0;
+                this.r.interimResults = !0;
+                this.r.lang = 'ja-JP';
+                this.e = e;
+                this.o = o;
+            }
+            start() {
+                this.r.onresult = e => {
+                    var s = [...e.results];
+                    var t = s[s.length-1];
+                    var q = {do:{s:''},key:{s:''}};
+                    var a = t[0].transcript;
+                    this.e.innerHTML = a;
+                    this.e.classList[t.isFinal?'add':'remove']('sc_final');
+                    if(t.isFinal) {
+                        for(var d of this.o.do) {
+                            var x = a.match(d.reg);
+                            if(x) {
+                                q.do.s = d.name;
+                                d.key && (q.do.k = true);
+                                break;
+                            }
+                        }
+                        for(var k of this.o.key) {
+                            var x = a.match(k.reg);
+                            if(x) {
+                                q.key.s = k.name;
+                                x[1] && (q.key.g = x[1]);
+                                break;
+                            }
+                        }
+                        var c = this.o.command[q.do.s];console.log(q,c);
+                        c && (
+                            q.do.k
+                                ? c[q.key.s] && (this.e.innerHTML += c[q.key.s](this,q.key.g))
+                                : (this.e.innerHTML += c(this))
+                        )
+                    }
+                }
+                this.r.onend = e => e.target.start();
+                this.r.start();
+            }
+            stop() {
+                this.r.onend = null;
+                this.r.stop();
+            }
+        },
         async(_,x,f,o,i,t,z)=>{
             (x = _.target.responseURL, x.match(/mime=audio%2Fweb[am]/) && (
                 i = (location.href.split(/\?|&/).find(v=>v.match(/^v=/))||'').replace(/^v=/,''),
